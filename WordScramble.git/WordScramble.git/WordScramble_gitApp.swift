98eadc7d5664566924496dff866fa8af71749e7f
Berardino Chiarello <berardino95@gmail.com>
2023-05-03 22:43:47 +0200

    First commit

---
+++ WordScramble.git/WordScramble.git/WordScramble_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramble_gitApp.swift
+//  WordScramble.git
+//
+//  Created by CHIARELLO Berardino - ADECCO on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScramble_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
